799e7a3475e56f58e20a7bed2169f26d8c961374
DerekYu <derek-nis@hotmail.com>
2019-04-07 18:31:15 -0400

    Jruby tests

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import subprocess
 import os
 
 FILES = {
-    'cruby': 'ruby.versions',
+#    'cruby': 'ruby.versions',
     'jruby': 'jruby.versions',
 }
 
@@ -35,29 +35,85 @@ def install_all_uninstalled_versions():
         subprocess.run(f'rvm install {version}', shell=True)
 
 def main():
-    install_all_uninstalled_versions()
+    # install_all_uninstalled_versions()
 
     # jruby breaks if we are not in the correct directory
     os.chdir('./optcarrot')
 
     for interpreter_type, versions in all_versions.items():
+        versions.reverse()
+
         for version in versions:
+
+            if interpreter_type == 'jruby':
+                # subprocess.run(['/bin/bash', '--login'], shell=True)
+                subprocess.run(f'rvm use {version}', shell=True)
+
             fpses = []
             for i in range(5):
 
                 benchmark_command = command(interpreter_type)
                 print(f'running result {i}')
-                result = subprocess.run(f'rvm {version} do {benchmark_command}', capture_output=True, shell=True)
+
+                if interpreter_type == 'cruby':
+                    result = subprocess.run(f'rvm {version} do {benchmark_command}',
+                            capture_output=True,
+                            shell=True)
+                else:
+                    result = subprocess.run(f'ruby {benchmark_command}',
+                            capture_output=True,
+                            shell=True)
 
                 if result.stderr:
                     continue
 
-                fps, checksum = result.stdout.decode('utf-8').strip('\n').split('\n')
+                fps = [output for output in
+                        result.stdout.decode('utf-8').strip('\n').split('\n')
+                        if 'fps' in output][0]
+
                 fpses.append(fps)
 
             print(f'Writing results back to file')
             with open(os.path.join('..', RESULTS), 'a') as f:
                 f.write(','.join([interpreter_type, version, *fpses]) + '\n')
 
+def run_jruby_specifically():
+    os.chdir('optcarrot')
+
+    jrubies = subprocess.run('rvm list strings | grep jruby', shell=True,
+            capture_output=True).stdout.decode('utf-8').strip('\n').split('\n')
+
+    # jruby < 1.7 cannot run the benchmark
+    jrubies = [version for version in jrubies
+            if 'jruby-9.1' in version or 'jruby-9.2' in version]
+
+    versions = {}
+    for jruby_version in jrubies:
+        versions[jruby_version] = []
+
+    for i in range(5):
+        print(f'running {i}')
+
+        jrubies_results = subprocess.run(
+                f'rvm {",".join(jrubies)} do '\
+                'bin/optcarrot --benchmark examples/Lan_Master.nes',
+                shell=True, capture_output=True)\
+                .stdout.decode('utf-8').strip('\n').split('\n')
+
+        # assume that the output order is the same as the running order
+        fps_results = [result for result in jrubies_results if 'fps' in result]
+
+        for fps_result, version in zip(fps_results, jrubies):
+            versions[version].append(fps_result)
+
+    os.chdir('..')
+
+    with open('jruby_results.csv', 'w+') as f:
+        for version, fps in versions.items():
+            f.write(f'jruby,{version},{",".join(fps)}\n')
+
+
 if __name__ == '__main__':
-    main()
+    # main()
+
+    run_jruby_specifically()
