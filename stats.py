e5f25a547fd5eb2360d8e431a5df3eba3be724ae
ArenBabikian <aren.babikian@mail.mcgill.ca>
2019-04-07 23:48:25 -0400

    versions finally sorting propperly

--- stats.py
+++ stats.py
@@ -3,128 +3,144 @@ import numpy as np
 import matplotlib.pyplot as plt
 from operator import itemgetter
 
-with open('results.csv') as f:
-	NOVERSION = -1
-	NOJUMP = -10
-	reader = csv.reader(f, delimiter=',')
-	crubyMeans = []
-	crubyStdDevs = []
-	crubyVersions = []
-	crubyJumps = []
-	sortedCrubyJumps = []
-	jrubyMeans = []
-	jrubyStdDevs = []
-	jrubyVersions = []
-	jrubyJumps = []
-	sortedJrubyJumps = []
-	for row in reader:
-		if len(row) == 7:
-			times = []
-			for i in range(2, 7):
-				timeString = row[i].split(" ")[1]
-				times.append(float(timeString))
+NOVERSION = -1
+NOJUMP = -10
+crubyMeans = []
+crubyStdDevs = []
+crubyVersions = []
+crubyJumps = []
+sortedCrubyJumps = []
+jrubyMeans = []
+jrubyStdDevs = []
+jrubyVersions = []
+jrubyJumps = []
+sortedJrubyJumps = []
 
-			#Basic stats calculation
-			if row[0] == "cruby":
-				crubyMeans.append(np.mean(times))
-				crubyStdDevs.append(np.std(times))
-				crubyVersions.append(row[1])
+for i in ['results.csv', 'jruby_results2.csv']:
+
+	with open(i) as f:
+
+		reader = csv.reader(f, delimiter=',')
+
+		if i == 'jruby_results2.csv' :
+			#try:
+			#	reader = sorted(reader, key = lambda s: list(map(int, str(s[1])[6:].split('.'))))
+			#except ValueError:
+			#	print("x")
+			#	reader = sorted(reader, key = lambda s: list(map(int, str(s[1])[6:].split('.'))))
+			reader = sorted(reader, key = lambda s: list(map(int, ((str((s[1].split('-'))[1])).split('.'))[:3])))
+
+			for i in reader:
+				print(i)
+
+		for row in reader:
+			if len(row) == 7:
+				times = []
+				for i in range(2, 7):
+					timeString = row[i].split(" ")[1]
+					times.append(float(timeString))
+
+				#Basic stats calculation
+				if row[0] == "cruby":
+					crubyMeans.append(np.mean(times))
+					crubyStdDevs.append(np.std(times))
+					crubyVersions.append(row[1])
+				else:
+					jrubyMeans.append(np.mean(times))
+					jrubyStdDevs.append(np.std(times))
+					jrubyVersions.append(row[1])
+				times.clear
 			else:
-				jrubyMeans.append(np.mean(times))
-				jrubyStdDevs.append(np.std(times))
-				jrubyVersions.append(row[1])
-			times.clear
-		else:
-			if row[0] == "cruby":
-				crubyMeans.append(NOVERSION)
-				crubyStdDevs.append(NOVERSION)
-				crubyVersions.append("x")
-			else:
-				jrubyMeans.append(NOVERSION)
-				jrubyStdDevs.append(NOVERSION)
-				jrubyVersions.append("x")
-	##cRuby
+				if row[0] == "cruby":
+					crubyMeans.append(NOVERSION)
+					crubyStdDevs.append(NOVERSION)
+					crubyVersions.append("x")
+				else:
+					jrubyMeans.append(NOVERSION)
+					jrubyStdDevs.append(NOVERSION)
+					jrubyVersions.append("x")
+##cRuby
 
-	#Get Jump Values
-	crubyJumps.append(NOJUMP)
-	for i in range(1, len(crubyMeans)) :
-		if (crubyMeans[i] != NOVERSION and crubyMeans[i-1] != NOVERSION) :
-			crubyJumps.append(crubyMeans[i] - crubyMeans[i-1])
-		else : 
-			crubyJumps.append(NOJUMP)
+#Get Jump Values
+crubyJumps.append(NOJUMP)
+for i in range(1, len(crubyMeans)) :
+	if (crubyMeans[i] != NOVERSION and crubyMeans[i-1] != NOVERSION) :
+		crubyJumps.append(crubyMeans[i] - crubyMeans[i-1])
+	else : 
+		crubyJumps.append(NOJUMP)
 
-	#Finding max jump versions
-	print("cRuby:")
-	sortedCrubyJumps = crubyJumps.copy()
-	sortedCrubyJumps.sort(reverse = True)
-	for i in range(0, 10) :
-		jumpVal = sortedCrubyJumps[i]
-		print(jumpVal, end=', ')
-		print(crubyVersions[crubyJumps.index(jumpVal)])
+#Finding max jump versions
+print("cRuby:")
+sortedCrubyJumps = crubyJumps.copy()
+sortedCrubyJumps.sort(reverse = True)
+for i in range(0, 10) :
+	jumpVal = sortedCrubyJumps[i]
+	print(jumpVal, end=', ')
+	print(crubyVersions[crubyJumps.index(jumpVal)])
 
-	##jRuby
+##jRuby
 
-	#Get Jump Values
-	jrubyJumps.append(NOJUMP)
-	for i in range(1, len(jrubyMeans)) :
-		if (jrubyMeans[i] != NOVERSION and jrubyMeans[i-1] != NOVERSION) :
-			jrubyJumps.append(jrubyMeans[i] - jrubyMeans[i-1])
-		else : 
-			jrubyJumps.append(NOJUMP)
+#Get Jump Values
+jrubyJumps.append(NOJUMP)
+for i in range(1, len(jrubyMeans)) :
+	if (jrubyMeans[i] != NOVERSION and jrubyMeans[i-1] != NOVERSION) :
+		jrubyJumps.append(jrubyMeans[i] - jrubyMeans[i-1])
+	else : 
+		jrubyJumps.append(NOJUMP)
 
-	#Finding max jump versions
-	print("jRuby:")
-	sortedJrubyJumps = jrubyJumps.copy()
-	sortedJrubyJumps.sort(reverse = True)
-	for i in range(0, 10) :
-		jumpVal = sortedJrubyJumps[i]
-		print(jumpVal, end=', ')
-		print(jrubyVersions[jrubyJumps.index(jumpVal)])
+#Finding max jump versions
+print("jRuby:")
+sortedJrubyJumps = jrubyJumps.copy()
+sortedJrubyJumps.sort(reverse = True)
+for i in range(0, 10) :
+	jumpVal = sortedJrubyJumps[i]
+	print(jumpVal, end=', ')
+	print(jrubyVersions[jrubyJumps.index(jumpVal)])
 
 
 
-	#All manipulations done
-	CST=4
+#All manipulations done
+CST=4
 
-	crubyX = [int(i)*CST for i in np.arange(0, len(crubyVersions)/CST)]
-	crubyVersionsPlot = itemgetter(*crubyX)(crubyVersions)
+crubyX = [int(i)*CST for i in np.arange(0, len(crubyVersions)/CST)]
+crubyVersionsPlot = itemgetter(*crubyX)(crubyVersions)
 
-	plt.plot(crubyMeans, 'ro')
-	plt.xticks(rotation=90)
-	plt.xticks(crubyX, crubyVersionsPlot)
-	plt.savefig("figRubyMeans.png")
-	plt.clf()
+plt.plot(crubyMeans, 'ro')
+plt.xticks(rotation=90)
+plt.xticks(crubyX, crubyVersionsPlot)
+plt.savefig("figRubyMeans.png")
+plt.clf()
 
-	plt.plot(crubyStdDevs, 'ro')
-	plt.xticks(rotation=90)
-	plt.xticks(crubyX, crubyVersionsPlot)
-	plt.savefig("figRubyStdDevs.png")
-	plt.clf()
+plt.plot(crubyStdDevs, 'ro')
+plt.xticks(rotation=90)
+plt.xticks(crubyX, crubyVersionsPlot)
+plt.savefig("figRubyStdDevs.png")
+plt.clf()
 
-	plt.plot(crubyJumps, 'ro')
-	plt.xticks(rotation=90)
-	plt.xticks(crubyX, crubyVersionsPlot)
-	plt.savefig("figRubyJumps.png")
-	plt.clf()
+plt.plot(crubyJumps, 'ro')
+plt.xticks(rotation=90)
+plt.xticks(crubyX, crubyVersionsPlot)
+plt.savefig("figRubyJumps.png")
+plt.clf()
 
-	jrubyX = [int(i)*CST for i in np.arange(0, len(jrubyVersions)/CST)]
-	jrubyVersionsPlot = itemgetter(*jrubyX)(jrubyVersions)
+jrubyX = [int(i)*CST for i in np.arange(0, len(jrubyVersions)/CST)]
+jrubyVersionsPlot = itemgetter(*jrubyX)(jrubyVersions)
 
-	plt.plot(jrubyMeans, 'ro')
-	plt.xticks(rotation=90)
-	plt.xticks(jrubyX, jrubyVersionsPlot)
-	plt.savefig("figJrubyMeans.png")
-	plt.clf()
+plt.plot(jrubyMeans, 'ro')
+plt.xticks(rotation=30)
+plt.xticks(jrubyX, jrubyVersionsPlot)
+plt.savefig("figJrubyMeans.png")
+plt.clf()
 
-	plt.plot(jrubyStdDevs, 'ro')
-	plt.xticks(rotation=90)
-	plt.xticks(jrubyX, jrubyVersionsPlot)
-	plt.savefig("figJrubyStdDevs.png")
-	plt.clf()
+plt.plot(jrubyStdDevs, 'ro')
+plt.xticks(rotation=90)
+plt.xticks(jrubyX, jrubyVersionsPlot)
+plt.savefig("figJrubyStdDevs.png")
+plt.clf()
 
-	plt.plot(jrubyJumps, 'ro')
-	plt.xticks(rotation=90)
-	plt.xticks(jrubyX, jrubyVersionsPlot)
-	plt.savefig("figJrubyJumps.png")
-	plt.clf()
+plt.plot(jrubyJumps, 'ro')
+plt.xticks(rotation=90)
+plt.xticks(jrubyX, jrubyVersionsPlot)
+plt.savefig("figJrubyJumps.png")
+plt.clf()
 	
